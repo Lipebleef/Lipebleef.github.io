073cc634d3e6707ab2b2989bf946580efa520355
Lipebleef <felipe.souzadasilva0404@gmail.com>
2025-10-21 23:44:33 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDFGvr00oIndPt4Eqow78Btiojt2pI8JT8",
+  authDomain: "gerenciador-de-gastos-80bc4.firebaseapp.com",
+  projectId: "gerenciador-de-gastos-80bc4",
+  storageBucket: "gerenciador-de-gastos-80bc4.appspot.com",
+  messagingSenderId: "139244624225",
+  appId: "1:139244624225:web:839220e0a69f12aa370d20"
+};
